a54cc184896cc4aee8dd6d409945db1720bb8f9d
Ariya Phengphon <81862069+ChanAndKlee@users.noreply.github.com>
2022-10-09 23:40:19 +0700

    Add Parallel Rank Sort from slide

--- ranksort.c
+++ ranksort.c
@@ -7,21 +7,36 @@
 
 int main()
 {
-    int lower = 0, upper = 9999, count = 1, amount = 10000;
+    int lower = 0, upper = 20, count = 1, amount = 5;
     int gen_num[amount];
     int check = 0;
 
     // set the starting value for the series of random ints
     srand(time(0));
 
-    #pragma omp parallel num_threads(4)
-    {  
+#pragma omp parallel num_threads(2)
+    {
         // random for {amount} ea
-        for(int i = 0; i < amount; i++) {
+        for (int i = 0; i < amount; i++)
+        {
             gen_num[i] = (rand() % (upper - lower + 1)) + lower;
             check += 1;
             printf("gen_num[%d]: %d\n", i, gen_num[i]);
         }
-        printf("%d", check);
+        printf("%d\n", check);
     }
+
+    // parallel rank sort (from slide)
+    // int x;
+    // forall(int i = 0; i < n; i++) {
+    //     x = 0;
+    //     for(int j = 0; j < n; j++) {
+    //         if(a[i] > a[j]) {
+    //             x++;    // count number less than it
+    //         }
+    //         else {
+    //             b[x] = a[i]; // copy number into correct place
+    //         }
+    //     }
+    // }
 }
