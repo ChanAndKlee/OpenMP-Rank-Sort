daf8e63df84f48c089b42ba2efab12cb2febcfea
Ariya Phengphon <81862069+ChanAndKlee@users.noreply.github.com>
2022-10-12 20:05:19 +0700

    Do what you told :P

--- main.c
+++ main.c
@@ -1,13 +1,11 @@
-/**
- *
- * @author Jun Han ( Johnson  )  Ooi
- * CISC 4335
- * Parallel Rank Sort
- *
- * Created on : April 30th 2014
- * Modified on : May 2nd 2014
- *
- * */
+// # ================================================= #
+// #    Project name: Parallel Ranksort via OpenMP     #
+// #               (Section 2)                         #
+// #   6388030  Kulawut     Makkamoltham               #
+// #   6388040  Ariya       Phengphon                  #
+// # ================================================= #
+
+// REF: https://github.com/tebesfinwo/CISC-4335-OpenMP-Rank-Sort-/blob/master/Parallel%20Rank%20Sort/main.c
 
 #include <omp.h>
 #include <stdio.h>
@@ -17,11 +15,18 @@
 #include <math.h>
 #include <sys/time.h>
 
-#define arraySize 10000 // define an array size to be sorted (e.g., 10000(4) , 100000(5) , 1000000(6) or 1M)
-#define numThreads 8    // define number of threads (e.g., 1, 4, 8, 12, and 16)
-
-int main()
+int main(int argc, char *argv[])
 {
+    // If argument counter is not equal to 3, then display here
+    if(argc != 3) {
+        printf("./filename <arraySizeToSort> <number of threads>\n");
+    }
+
+    int arraySize, numThreads;
+    
+    // Convert argv[1], argv[2] to integers using atoi() to do Integer conversion
+    arraySize = atoi(argv[1]);
+    numThreads = atoi(argv[2]);
 
     double openmp_start, openmp_end;
     struct timeval stop, start;
@@ -29,7 +34,7 @@ int main()
     int i, j;
     int int_max = 10000;    // define ranging from 0 - 9999
 
-    int arr[arraySize]; // array to be sorted
+    int arr[arraySize];     // array to be sorted
     int sorted[arraySize];
     int linearlySorted[arraySize];
 
@@ -37,17 +42,13 @@ int main()
 
     srand(time(NULL));
 
-    /*
-     * Random the number
-     */
+    // Random the number
     for (i = 0; i < arraySize; i++)
     {
         arr[i] = rand() % int_max;
     }
 
-    /*
-     * Sequential Rank Sort
-     */
+    // Sequential Rank Sort
     gettimeofday(&start, NULL);
     for (i = 0; i < arraySize; i++)
     {
@@ -62,16 +63,12 @@ int main()
 
     gettimeofday(&stop, NULL);
 
-    /*
-     * Parallel Rank sort using OpenMP
-     */
+    // Parallel Rank sort using OpenMP
     openmp_start = omp_get_wtime();
 
     omp_set_num_threads(numThreads);
 
-    /*
-     * Rank sort with OpenMP
-     */
+    // Rank sort with OpenMP
     #pragma omp parallel for private(j)
     for (i = 0; i < arraySize; i++)
     {
@@ -91,10 +88,8 @@ int main()
         printf("unsorted : %d, Parallel sorted : %d, Sequential sorted : %d\n", arr[i], sorted[i], linearlySorted[i]);
     }
 
-    printf(">> Time used in parallel rank sort using openmp : %f seconds\n", openmp_end - openmp_start);
+    printf(">> Time used in parallel rank sort using OpenMP : %f seconds\n", openmp_end - openmp_start);
     printf(">> Time used in sequential rank sort : %f seconds\n", (stop.tv_sec - start.tv_sec) + (stop.tv_usec - start.tv_usec) / 1000000.0f);
-    // Change this to print the Speed up
-    // printf(">> Time Difference between parallel vs sequential rank sort : %f\n", fabs(difftime(seq_end, seq_start) - (openmp_end - openmp_start)));
-    // 
+    printf(">> Speed Up (sequential / parallel) : %f\n", (openmp_end - openmp_start) / ((stop.tv_sec - start.tv_sec) + (stop.tv_usec - start.tv_usec) / 1000000.0f) );
     return 0;
 }
